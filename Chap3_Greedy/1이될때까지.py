ece788b1f0f2aec2cab8429661a358191e065b6c
yhLee11 <mrncd1@gmail.com>
2022-10-09 00:02:36 +0900

    Greedy Algorithm

--- Chap3_Greedy/1이될때까지.py
+++ Chap3_Greedy/1이될때까지.py
@@ -16,11 +16,13 @@ print(cnt)
 n,k=25,5
 res=0
 while True:
+    #(n==k로 나누어떨어지는 수)가 될 때까지 1씩 빼기
     target=(n//k)*k
     res+=(n-target)
     n=target
-
+    #n이 k보다 작을때(더 이상 나눌 수 없을 때) break
     if n<k:break
+    #k로 나누기
     res+=1
     n//=k
 res+=(n-1)
